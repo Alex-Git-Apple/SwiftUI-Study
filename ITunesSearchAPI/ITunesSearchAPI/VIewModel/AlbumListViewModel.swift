9aa6276215c97b426cf1dd49382f911a437d1544
Pin <just4sport30@gmail.com>
2024-02-14 10:12:34 -0800

    Add pagination to fetch album API.

--- ITunesSearchAPI/ITunesSearchAPI/VIewModel/AlbumListViewModel.swift
+++ ITunesSearchAPI/ITunesSearchAPI/VIewModel/AlbumListViewModel.swift
@@ -8,39 +8,69 @@
 import Combine
 import Foundation
 
-//https://itunes.apple.com/search?term=jack+johnson&entity=album&limit=20
+//https://itunes.apple.com/search?term=jack+johnson&entity=album&limit=20&offset=10
 //https://itunes.apple.com/search?term=jack+johnson&entity=song&limit=20
 //https://itunes.apple.com/search?term=jack+johnson&entity=movie&limit=20
+
+enum ITSState: Comparable {
+    case normal
+    case loading
+    case loadedAll
+    case error(String)
+}
 
 @MainActor
 class AlbumListViewModel: ObservableObject {
     
     @Published var searchItem: String = ""
     @Published var albums: [Album] = []
+    @Published var state: ITSState = .normal
     
-    private var cancellables = Set<AnyCancellable>()
+    private var subscriptions = Set<AnyCancellable>()
     
     let limit: Int = 20
+    var page: Int = 0
+    var offset: Int {
+        page * limit
+    }
     
     init() {
         $searchItem
             .dropFirst()
             .debounce(for: .seconds(0.5), scheduler: RunLoop.main)
-            .sink { [weak self] term in
+            .sink { [weak self] text in
                 guard let self = self else { return }
+                self.state = .normal
                 Task {
-                    await self.fetchAlbums(for: term)
+                    self.albums = []
+                    await self.fetchAlbums(for: text)
                 }
             }
-            .store(in: &cancellables)
+            .store(in: &subscriptions)
+    }
+    
+    func loadData() async {
+        await fetchAlbums(for: searchItem)
     }
     
     func fetchAlbums(for searchTerm: String) async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=\(searchItem)n&entity=album&limit=\(limit)") else { return }
+        
+        guard !searchTerm.isEmpty else { return }
+        guard let url = URL(string: "https://itunes.apple.com/search?term=\(searchItem)n&entity=album&limit=\(limit)&offset=\(offset)") else { return }
+        guard self.state == .normal else { return }
         do {
+            self.state = .loading
+            
             let (data, _) = try await URLSession.shared.data(from: url)
             let result = try JSONDecoder().decode(AlbumResult.self, from: data)
-            self.albums = result.results
+            self.albums.append(contentsOf: result.results)
+            
+            if result.resultCount == limit {
+                self.state = .normal
+                page += 1
+            } else {
+                self.state = .loadedAll
+            }
         } catch (let error ){
             print("error: \(error).")
         }
--- ITunesSearchAPI/ITunesSearchAPI/View/AlbumListView.swift
+++ ITunesSearchAPI/ITunesSearchAPI/View/AlbumListView.swift
@@ -12,15 +12,40 @@ struct AlbumListView: View {
     @StateObject var viewModel = AlbumListViewModel()
     
     var body: some View {
+        
+        
         NavigationView {
             List {
                 ForEach(viewModel.albums) { album in
                     Text(album.collectionName)
                 }
+                
+                switch viewModel.state {
+                case .normal:
+                    Color.clear
+                        .task {
+                            await viewModel.loadData()
+                        }
+                case .loading:
+                    ProgressView()
+                        .progressViewStyle(.circular)
+                case .loadedAll:
+                    Text("No more!")
+                case .error:
+                    EmptyView()
+                }
             }
             .listStyle(.plain)
             .searchable(text: $viewModel.searchItem)
+           
         }
+        
+    }
+}
+
+extension View {
+    func hidden(_ shouldHide: Bool = true) -> some View {
+        opacity(shouldHide ? 0 : 1)
     }
 }
 
